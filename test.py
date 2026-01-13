39c7d989d960f30eaa66ad1a9175a2f8924f9742
Noa Nitzan <noanitzan99@gmail.com>
2026-01-13 15:40:52 +0200

    initial commit

---
+++ test.py
@@ -0,0 +1,24 @@
+import os
+from question_3 import write_random_numbers
+
+def test_number_of_lines():
+    filename = "test_numbers.txt"
+    write_random_numbers(5, filename)
+
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    assert len(lines) == 5
+    os.remove(filename)
+
+
+def test_numbers_in_range():
+    filename = "test_numbers.txt"
+    write_random_numbers(10, filename)
+
+    with open(filename, "r") as f:
+        for line in f:
+            num = int(line.strip())
+            assert 1 <= num <= 500
+
+    os.remove(filename)
